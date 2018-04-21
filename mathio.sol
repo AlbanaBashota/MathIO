a96a098852fccde9a7c81e6f004d69f6cfcda7ad
erini <erin.godanci@gmail.com>
2018-04-21 04:18:37 -0500

    dergimi i kontrates

---
+++ mathio.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.0; contract MathIO{
+    function add(int x, int y)public constant returns (int z) {
+        require((z = x + y) >= x);
+    }
+    function sub(int x, int y) public constant returns (int z) {
+        require((z = x - y) <= x);
+    }
+    function mul(int x, int y)public constant returns (int z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+    function min(int x, int y) public constant returns (int z) {
+        return x <= y ? x : y;
+    }
+    function max(int x, int y) public constant returns (int z) {
+        return x >= y ? x : y;
+    }
+    function fact(int a)public constant returns(int){
+        int f = 1;
+        for(int i = 0 ; i < a ; i++ ){
+            f = f *(a-i);
+        }
+        return f;
+    }
+}
